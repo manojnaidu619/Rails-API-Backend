6677df7c879c4af0069a6a568734ebb031fb8ddf
Manoj Naidu <manojnaidu619@gmail.com>
2018-08-02 15:57:42 +0530

    Added Create acion

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -2,4 +2,19 @@ class BanksController < ApplicationController
   def index
     render json: Bank.all
   end
+
+  def create
+    @bank = Bank.new(bank_params)
+     if @bank.save
+       render json: {status: "SUCCESS", message: "Loaded Data", data: @bank}
+     else
+       render json: @bank.errors, status: 422
+     end
+  end
+
+  private
+
+  def bank_params
+    params.require(:bank).permit(:name, :cash)
+  end
 end
